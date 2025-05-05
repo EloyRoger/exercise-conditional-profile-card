5dde9c2629ebbcda8e4b473f9d1905a6a1959040
EloyR <eloy.roger.araya@gmail.com>
2025-05-05 18:13:04 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -37,10 +37,10 @@ function render(variables = {}) {
           <h2>${variables.rol}</h2>
           <h3>${variables.city} ${variables.country}</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://x.com/roger_eloy69627"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/EloyRoger"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/eloy-roger-araya-71b8a9264/"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/accounts/onetap/?next=%2F"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
